6f1895feab3b9815522d8c47992ccbf2a8194f31
Vinay Kumar Bandlamudi - b0k03k3 <bandlamudi.kumar@walmart.com>
2025-06-14 20:28:57 +0530

    Application Base setup

---
+++ src/main/java/com/java/practise/JavaSpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.java.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootPracticeApplication.class, args);
+	}
+
+}
